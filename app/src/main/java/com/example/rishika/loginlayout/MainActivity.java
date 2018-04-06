0e8167b92a2c165b59c6cf53a3dd4be4f4befeb1
rishikagupta2437 <rishikagupta2013@gmail,com>
2018-04-06 15:47:52 +0530

    login page using linear layout

---
+++ app/src/main/java/com/example/rishika/loginlayout/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.rishika.loginlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
